6f34525fb4a8b44e87e63934aeada0a09daa4f36
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-07-24 13:41:08 -0500

    set url api AdonisJs from aws

--- libraries/env.js
+++ libraries/env.js
@@ -1,13 +1,15 @@
 
 export function getUrlEstadoCuenta()
 {
-    return 'http://34.71.156.184/ucom/estadocuenta';
+    //return 'http://34.71.156.184/ucom/estadocuenta';
+    return 'https://caa92a61cb594be182aafad3da8e7a08.vfs.cloud9.us-east-1.amazonaws.com/ucom/estadocuenta';
 }
 
 
 export function getUrlPago()
 {
-    return 'http://34.71.156.184/ucom/registrarpago';
+    //return 'http://34.71.156.184/ucom/registrarpago';
+    return 'https://caa92a61cb594be182aafad3da8e7a08.vfs.cloud9.us-east-1.amazonaws.com/ucom/registrarpago';
 }
 
 
