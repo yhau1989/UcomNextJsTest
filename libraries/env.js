a3d3607bf1450fd0728aef5228e9391516a0fed7
Samuel Pilay Muñoz <yhau1989@gmail.com>
2020-07-24 20:45:07 -0500

    url de api DigitalOcean

--- libraries/env.js
+++ libraries/env.js
@@ -2,14 +2,14 @@
 export function getUrlEstadoCuenta()
 {
     //return 'http://34.71.156.184/ucom/estadocuenta';
-    return 'https://caa92a61cb594be182aafad3da8e7a08.vfs.cloud9.us-east-1.amazonaws.com/ucom/estadocuenta';
+    return 'https://devmania.shop/ucom/estadocuenta';
 }
 
 
 export function getUrlPago()
 {
     //return 'http://34.71.156.184/ucom/registrarpago';
-    return 'https://caa92a61cb594be182aafad3da8e7a08.vfs.cloud9.us-east-1.amazonaws.com/ucom/registrarpago';
+    return 'https://devmania.shop/ucom/registrarpago';
 }
 
 
